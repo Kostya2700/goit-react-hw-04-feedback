28f6913ac290c975b23250a767851c3504752ea5
kostya <kostizavali@gmail.com>
2022-10-26 15:54:35 +0300

    task2

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,36 +1,45 @@
 import React from 'react';
+import { Statisticks } from './Statisticks/Statisticks';
 
 class Counter extends React.Component {
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
-    total: 0,
   };
-  goodFeedback = e => {
+  percentage = 0;
+  goodFeedback = () => {
     this.setState(p => {
       return { good: p.good + 1 };
     });
   };
-  neutralFeedback = e => {
+  neutralFeedback = () => {
     this.setState(p => {
       return { neutral: p.neutral + 1 };
     });
   };
-  badFeedback = e => {
+  badFeedback = () => {
     this.setState(p => {
       return { bad: p.bad + 1 };
     });
   };
+  countTotalFeedbacks() {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+  countPositiveFeedbackPercentage() {
+    const totalFeedbacks = this.countTotalFeedbacks();
+    const goSt = this.state.good;
+    return Math.round((goSt * 100) / totalFeedbacks);
+  }
 
   render() {
+    this.total = this.countTotalFeedbacks.call(this);
+    this.percentage = this.countPositiveFeedbackPercentage.call(this);
     return (
       <div className="div">
-        <h2 className="h2">{this.state.total}</h2>
-        <span className="Counter_good">{this.state.good}</span>
-        <span className="Counter_neutral">{this.state.neutral}</span>
-        <span className="Counter_bad">{this.state.bad}</span>
-        <div className="Counter_controls">
+        <h2 className="h2">Please leave feedback</h2>
+        <div className="counter_controls">
           <button type="button" onClick={this.goodFeedback}>
             good
           </button>
@@ -40,6 +49,20 @@ class Counter extends React.Component {
           <button type="button" onClick={this.badFeedback}>
             bad
           </button>
+        </div>
+        <h2 className="h2">Statisticks</h2>
+        <div>
+          {this.total ? (
+            <Statisticks
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.total}
+              percentage={this.percentage}
+            ></Statisticks>
+          ) : (
+            <h3>No feedback given </h3>
+          )}
         </div>
       </div>
     );
